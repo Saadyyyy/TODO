7284c34c91cec6ad972b0038a17a76d7f8dc89a0
Saadyyyy <laodesaady12345@gmail.com>
2024-02-13 17:48:26 +0700

    Fix bug tugas controller for unit testing

--- api/controller/tugas_controller.go
+++ api/controller/tugas_controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"Todo/api/service"
+	respons "Todo/api/service/Respons"
+	"Todo/models"
 	"Todo/utils/helper"
 	"net/http"
 	"strconv"
@@ -31,14 +33,14 @@ func (uc *TugasController) GetAll(ctx *gin.Context) {
 }
 
 func (uc *TugasController) GetByID(ctx *gin.Context) {
-	_, err := strconv.Atoi(ctx.Param("id"))
+	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"massage": "Error",
 			"Data":    err,
 		})
 	}
-	data, err := uc.TugasService.GetById(ctx)
+	data, err := uc.TugasService.GetById(id)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"massage": "Error",
@@ -53,7 +55,10 @@ func (uc *TugasController) GetByID(ctx *gin.Context) {
 }
 
 func (uc *TugasController) Create(ctx *gin.Context) {
-	data, err := uc.TugasService.Create(ctx)
+	tugas := &models.Tugas{}
+	ctx.ShouldBindJSON(&tugas)
+	data, err := uc.TugasService.Create(tugas)
+
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"massage": "Status Internal Server Error",
@@ -70,8 +75,26 @@ func (uc *TugasController) Create(ctx *gin.Context) {
 }
 
 func (uc *TugasController) Update(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid ID",
+			"error":   err.Error(),
+		})
+		return
+	}
+	tugas := respons.UpdateTugasRespon{}
+	if err := ctx.ShouldBindJSON(&tugas); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid request body",
+			"error":   err.Error(),
+		})
+		return
+	}
 
-	data, err := uc.TugasService.Update(ctx)
+	data, err := uc.TugasService.Update(id, tugas)
+
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"massage": "Status Internal Server Error",
@@ -87,7 +110,16 @@ func (uc *TugasController) Update(ctx *gin.Context) {
 	})
 }
 func (uc *TugasController) Delete(ctx *gin.Context) {
-	data, err := uc.TugasService.Delete(ctx)
+	idStr := ctx.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid ID",
+			"error":   err.Error(),
+		})
+		return
+	}
+	data, err := uc.TugasService.Delete(id)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"massage": "Status Internal Server Error",
@@ -114,7 +146,7 @@ func (uc *TugasController) GetByStatus(ctx *gin.Context) {
 	}
 
 	// Memanggil metode GetByStatus dari service
-	result, err := uc.TugasService.GetByStatus(ctx, status, page, perPage)
+	result, err := uc.TugasService.GetByStatus(status, page, perPage)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -134,7 +166,7 @@ func (uc *TugasController) GetBylevel(ctx *gin.Context) {
 	deaGetBylevel := ctx.Param("level")
 
 	// Memanggil metode GetBylevel dari service
-	result, err := uc.TugasService.GetBylevel(ctx, deaGetBylevel, page, perPage)
+	result, err := uc.TugasService.GetBylevel(deaGetBylevel, page, perPage)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -154,7 +186,7 @@ func (uc *TugasController) GetByDeadline(ctx *gin.Context) {
 	deaGetBylevel := ctx.Param("deadline")
 
 	// Memanggil metode GetBylevel dari service
-	result, err := uc.TugasService.GetByDeadline(ctx, deaGetBylevel, page, perPage)
+	result, err := uc.TugasService.GetByDeadline(deaGetBylevel, page, perPage)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
